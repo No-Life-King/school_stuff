fdcb7235f9f63aae1da04c713daf541045c9a5f9
Phil <nolife.king88@gmail.com>
2016-12-28 22:24:25 -0500

    commented

--- python/fraction.py
+++ python/fraction.py
@@ -1,14 +1,23 @@
 def gcf(m, n):
+    """
+    A helper method that allows fraction objects to find the greatest
+    common factor of the numbers 'm' and 'n'.
+    """
     while m%n != 0:
         old_m = m
         old_n = n
         m = old_n
         n = old_m % old_n
+    
     return n
 
     
 class Fraction:
-    
+    """
+    A partial implementation of a class that handles fractions. There
+    is emphasis here on overriding python builtin methods to allow
+    operators and language features to handle fractions properly.
+    """
     
     def __init__(self, top, bottom):
         self.numerator = top
