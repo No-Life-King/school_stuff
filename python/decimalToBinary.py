1e24d4ffac82866a02db6c96e7e211a8d3d0c60e
Phil <nolife.king88@gmail.com>
2016-12-28 21:29:37 -0500

    commented "decimalToBinary.py" and "Stack.py"

--- python/decimalToBinary.py
+++ python/decimalToBinary.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 from Stack import Stack
+"""
+This script uses a stack to implement a method of converting a decimal number
+to a binary string. The method entails dividing the decimal number by 2
+repeatedly. If there is a remainder, then a binary '1' is pushed to the stack,
+otherwise a zero. A stack makes sense in this senario because it can be used
+to efficiently reverse the binary number at the end of the process.
+"""
 
 def decimal_to_binary(decimal_number):
+    """
+    Converts a decimal number to binary using the 'divide-by-2' method.
+    """
     binary_stack = Stack()
     while decimal_number > 0:
         binary_digit = decimal_number % 2
@@ -14,5 +24,7 @@ def decimal_to_binary(decimal_number):
         
     return binary_string
 
-print(decimal_to_binary(233))
-print(decimal_to_binary(1024))
+
+# a couple of tests for this function
+print(decimal_to_binary(233))       # should return 11101001
+print(decimal_to_binary(1024))      # should return 10000000000
