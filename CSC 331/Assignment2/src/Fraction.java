0b6f21b70d0ac587041cb207c51da479b85f5ed5
Unknown <nolife.king88@gmail.com>
2017-09-07 17:37:36 -0400

    Finished Assignment2 for CSC331

--- CSC 331/Assignment2/src/Fraction.java
+++ CSC 331/Assignment2/src/Fraction.java
@@ -14,13 +14,14 @@ public class Fraction {
 	// Creates an empty fraction object whose numerator and denominator can be set later.
 	public Fraction() {}
 	
-	// Creates a new fraction object by converting a double to a fraction
+	// Creates a new fraction object by converting a double to a fraction.
 	public Fraction(double x) {
-		// go by powers of 10 to 
+		// multiply by "well chosen one" powers of 10 until a whole number numerator is reached 
         int y = 10;
         Double multipleOfX = new Double(x);
         Double powerOf10 = new Double(10);
         
+        // stored as 2 integers, the fraction values cannot exceed the maximum integer value
         while (powerOf10 < Integer.MAX_VALUE) {
             multipleOfX = x * powerOf10;
             y = multipleOfX.intValue();
@@ -35,17 +36,22 @@ public class Fraction {
             	powerOf10 *= 10;
             }
         }
+        
+        // once a fitting multiple is found, set the numerator and denominator and reduce the fraction
         numerator = y;
         denominator = powerOf10.intValue();
         reduce();
 	}
 	
+	// Sets the numerator and denominator and then reduces the fraction.
 	public Fraction(int a, int b) {
 		numerator = a;
 		denominator = b;
-                reduce();
-        }
+        reduce();
+	}
 	
+	// Employs Euclid's algorithm to find the greatest common divisor of the 
+	// numerator and denominator and then factors that divisor out. 
 	public void reduce() {
 		int temp;
 		int a = numerator;
@@ -56,10 +62,10 @@ public class Fraction {
 			a = temp;
 		}
                 
-                if (a != 0 || a != 1) {
-                    numerator /= a;
-                    denominator/= a;
-                }
+        if (a != 0 || a != 1) {
+            numerator /= a;
+            denominator/= a;
+        }
 	}
 	
 	public int getNumerator() {
@@ -78,6 +84,7 @@ public class Fraction {
 		denominator = b;
 	}
 	
+	// Adds this fraction to another fraction, returning a new fraction that is the sum of the two.
 	public Fraction add(Fraction other) {
 		Fraction sum = new Fraction();
 		
@@ -91,22 +98,29 @@ public class Fraction {
 		return sum;
 	}
 	
+	// Subtracts another fraction from this fraction, returning a new fraction that is the difference of the two.
 	public Fraction sub(Fraction other) {
+		// add the opposite of the other fraction
 		Fraction opposite = new Fraction(other.getNumerator() * -1, other.getDenominator());
 		
 		return add(opposite);
 	}
 	
+	// Multiplies this fraction by another fraction, returning a new fraction that is the product of the two.
 	public Fraction mult(Fraction other) {
 		Fraction multiplied = new Fraction(numerator * other.getNumerator(), denominator * other.getDenominator());
 		return multiplied;
 	}
 	
+	// Divides this fraction by another fraction, returning a new fraction.
 	public Fraction div(Fraction other) {
+		// simply multiply by the reciprocal 
 		Fraction divided = new Fraction(numerator * other.getDenominator(), denominator * other.getNumerator());
 		return divided;
 	}
 	
+	// Compares this fraction to another fraction, returning 1 if this fraction is greater, -1
+	// if this fraction is lower, and 0 if the two fractions are equal.
 	public int compareTo(Fraction other) {
 		if (this.toDecimal() > other.toDecimal()) {
 			return 1;
@@ -117,12 +131,14 @@ public class Fraction {
 		}
 	}
 	
+	// Returns the double approximation of this fraction.
 	public double toDecimal() {
             double a = this.numerator;
             double b = this.denominator;
             return a/b;
 	}
 	
+	// Allow the fraction to be printed in a nice way.
 	@Override
 	public String toString() {
 		return numerator + "/" + denominator;
