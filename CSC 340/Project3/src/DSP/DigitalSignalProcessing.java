e69fde05604dd49ff261456fcf9d694f954de9e8
Unknown <nolife.king88@gmail.com>
2017-12-06 11:57:47 -0500

    Finished commenting project 3

--- CSC 340/Project3/src/DSP/DigitalSignalProcessing.java
+++ CSC 340/Project3/src/DSP/DigitalSignalProcessing.java
@@ -17,11 +17,16 @@ public class DigitalSignalProcessing {
 	public static void main(String[] args) {
 
 		// test classes and methods if set to 'true'
-		// runTests(true);
+		runTests(true);
+
+		/**
+		 * The following method calls can be used to apply digital signal processing techniques
+		 * to generate the requested project data.
+		 */
 
 		// filters();
 
-		// phaseShift(0);
+		// phaseShift(0);		// arg is the position of the pulse (0-1023)
 
 		// correlation();
 
@@ -29,58 +34,18 @@ public class DigitalSignalProcessing {
 
 		// DTMFtones();
 
-		twoDimensionalSignalCorrelation();
-
-		/*
-		 * The following code uses the estimated piecewise function to generate samples for a Fast Fourier
-		 * Transform, which can then be used to print out the power spectral density of the frequencies.
-		 * This code can be quickly modified to use the 'sineFunction' defined in its own method below for
-		 * generating an aribtrary number of even or odd frequencies and samples.
-		 */
-
-		/*
-		double[] oddSamples = piecewiseFunction(512, true);
-		double[] evenSamples = piecewiseFunction(512, false);
-
-		ComplexNumber[] oddTransform = fastFourierTransform(convertToComplex(oddSamples), false);
-		ComplexNumber[] evenTransform = fastFourierTransform(convertToComplex(evenSamples), false);
-
-		for (int x=0; x<50; x++) {
-			 System.out.println(Math.pow(evenTransform[x].magnitude(), 2));
-		}
-		*/
-
-		/*
-		 * The following code examines two sine functions with no amplitude or phase shift changes. The functions
-		 * are sin(2pi(11)t) and sin(2pi(23)t). The are both added and multiplied for the purpose of examining the
-		 * effects on the power spectral density.
-		 */
-
-		/*
-		double[] sumOfSines = new double[1024];
-		for (int x=0; x<1024; x++) {
-			sumOfSines[x] = Math.sin(2 * Math.PI * 11 * x/1024.0) + Math.sin(2 * Math.PI * 23 * x/1024.0);
-		}
-
-		double[] productOfSines = new double[1024];
-		for (int x=0; x<1024; x++) {
-			productOfSines[x] = Math.sin(2 * Math.PI * 11 * x/1024.0) * Math.sin(2 * Math.PI * 23 * x/1024.0);
-		}
-
-		ComplexNumber[] sumTransform = fastFourierTransform(convertToComplex(sumOfSines), false);
-		ComplexNumber[] productTransform = fastFourierTransform(convertToComplex(productOfSines), false);
-
-		for (int x=0; x<1024; x++) {
-			// System.out.println(Math.pow(productTransform[x].magnitude(), 2));
-		}
-		*/
+		 twoDimensionalSignalCorrelation();
 
 	}
 
+	/**
+	 * Performs data processing related to FFT correlation and convolution.
+	 */
 	private static void correlation() {
 		ArrayList<Double> pulse = new ArrayList<Double>();
 		ArrayList<Double> signal = new ArrayList<Double>();
 
+		// read in the pulse data and received signal data from a text file
 		try {
 			BufferedReader in = new BufferedReader(new FileReader("rangeTestDataF2017.txt"));
 			String line;
@@ -113,20 +78,27 @@ public class DigitalSignalProcessing {
 			pulseArray[x] = pulse.get(x);
 		}
 
+		// generate a wave that represents the correlation strength of the data
 		ComplexNumber[] results = correlateSignals(pulseArray, signalArray);
 
 		for (ComplexNumber result: results) {
 			//System.out.println(result.getReal());
 		}
 
+		/**
+		 * Below is some code for using FFT convolution to smooth a signal.
+		 */
 		double[] avgFilter = new double[1024];
 		double oneTenth = 1.0/10;
 
+		// populate a filter for the signal
 		for (int x=0; x<10; x++) {
 			avgFilter[x] = oneTenth;
 			avgFilter[avgFilter.length - x - 1] = oneTenth;
 		}
 
+		// the algorithm for FFT convolution involves multiplying the FFT of the signal by the
+		// FFT of the filter and then taking the inverse FFT of the resulting product
 		ComplexNumber[] signalFFT = fastFourierTransform(convertToComplex(signalArray), false);
 		ComplexNumber[] filterFFT = fastFourierTransform(convertToComplex(avgFilter), false);
 		ComplexNumber[] multiplied = new ComplexNumber[1024];
@@ -137,32 +109,48 @@ public class DigitalSignalProcessing {
 
 		ComplexNumber[] convoluted = fastFourierTransform(multiplied, true);
 
-		for (int x=0; x<256; x++) {
-			System.out.println(signalArray[x]);
+		// print some results centered at the received ping echo
+		for (int x=251; x<507; x++) {
+			// System.out.println(x);
 		}
 
 	}
 
+	/**
+	 * Compares two signals to see how much they correlate.
+	 * @param pulseArray An array containing pulse data padded with zeros to the same length as the sample signal.
+	 * @param signalArray The samples of a signal.
+	 * @return An array of complex numbers that signify the correlation strength of the two waves.
+	 */
 	private static ComplexNumber[] correlateSignals(double[] pulseArray, double[] signalArray) {
+
+		// take the FFT of both signals
 		ComplexNumber[] signalFFT = fastFourierTransform(convertToComplex(signalArray), false);
 		ComplexNumber[] pulseFFT = fastFourierTransform(convertToComplex(pulseArray), false);
 
+		// take the conjugate of all complex pulse data
 		for (int x=0; x<pulseFFT.length; x++) {
 			pulseFFT[x] = pulseFFT[x].conjugate();
 		}
 
 		ComplexNumber[] correlation = new ComplexNumber[signalFFT.length];
 
+		// multiply the signal data by the conjugate pulse data
 		for (int x=0; x<signalFFT.length; x++) {
 			correlation[x] = signalFFT[x].multiply(pulseFFT[x]);
 		}
 
+		// return the inverse FFT of the product
 		return fastFourierTransform(correlation, true);
 	}
 
+	/**
+	 * Correlates two images that have been converted to signals.
+	 */
 	private static void twoDimensionalSignalCorrelation() {
-		Picture testSignal = new Picture(512, 512, "Test Signal");
 
+		// draw a test signal image
+		Picture testSignal = new Picture(512, 512, "Test Signal");
 		double[][] testSignalPixels = new double[512][512];
 
 		for (int i=0; i<512; i++) {
@@ -189,10 +177,11 @@ public class DigitalSignalProcessing {
 			}
 		}
 
+		// display the image
 		testSignal.show();
 
+		// draw a test pulse image
 		Picture testPulse = new Picture(512, 512, "Test Pulse");
-
 		double[][] testPulsePixels = new double[512][512];
 
 		for (int i=0; i<50; i++) {
@@ -213,6 +202,7 @@ public class DigitalSignalProcessing {
 			}
 		}
 
+		// display the image
 		testPulse.show();
 
 		ComplexNumber[][] complexSignal = new ComplexNumber[512][512];
@@ -230,18 +220,22 @@ public class DigitalSignalProcessing {
 			}
 		}
 
+		// take the FFT of both the test image data and the test pulse data
 		ComplexNumber[][] signalFFT = twoDimensionalFFT(complexSignal, false);
 		ComplexNumber[][] pulseFFT = twoDimensionalFFT(complexPulse, false);
 		ComplexNumber[][] multiplied = new ComplexNumber[512][512];
 
+		// multiply the image FFT data by the conjugate of the pulse FFT data
 		for (int i=0; i<512; i++) {
 			for (int j=0; j<512; j++) {
 				multiplied[i][j] = pulseFFT[i][j].conjugate().multiply(signalFFT[i][j]);
 			}
 		}
 
+		// take the 2D inverse FFT of the product
 		ComplexNumber[][] correlated = twoDimensionalFFT(multiplied, true);
 
+		// find the maximum correlation magnitude
 		double maxMagnitude = 0;
 
 		for (int i=0; i<512; i++) {
@@ -253,6 +247,7 @@ public class DigitalSignalProcessing {
 			}
 		}
 
+		// display everything within 10% of the maximum in red
 		double[][] redmask = new double[512][512];
 		double threshold = 0.9 * maxMagnitude;
 
@@ -268,10 +263,11 @@ public class DigitalSignalProcessing {
 		Picture red = new Picture(512, 512, "Red Mask");
 		Picture correlationMagnitudes = new Picture(512, 512, "Correlation Magnitudes");
 
+		// render the correlated image using a log-linear scaling technique
 		for (int i=0; i<512; i++) {
 			for (int j=0; j<512; j++) {
-				correlationMagnitudes.set(i, j, (int) (Math.log(correlated[i][j].magnitude() + 1) / Math.log(1.08)));
-				red.set(i, j, (int) (Math.log(correlated[i][j].magnitude() + 1) / Math.log(1.08)));
+				correlationMagnitudes.set(i, j, (int) (Math.log(correlated[i][j].magnitude() + 1) / Math.log(2) * 9));
+				red.set(i, j, (int) (Math.log(correlated[i][j].magnitude() + 1) / Math.log(2) * 9));
 			}
 		}
 
@@ -285,15 +281,21 @@ public class DigitalSignalProcessing {
 			}
 		}
 
-		correlationMagnitudes.show();
 		red.show();
 
 	}
 
+	/**
+	 * Runs a fast fourier transform on a two dimensional array.
+	 * @param matrix A 2D array of complex numbers.
+	 * @param inverse Set to true if an inverse 2D fast fourier transform should be performed.
+	 * @return The transformed data.
+	 */
 	private static ComplexNumber[][] twoDimensionalFFT(ComplexNumber[][] matrix, boolean inverse) {
 		int len = matrix.length;
 		ComplexNumber[][] transformed = new ComplexNumber[len][len];
 
+		// transform all the rows
 		for (int i=0; i<len; i++) {
 			ComplexNumber[] complexRow = new ComplexNumber[len];
 
@@ -309,6 +311,7 @@ public class DigitalSignalProcessing {
 
 		}
 
+		// transform all the columns
 		for (int j=0; j<len; j++) {
 			ComplexNumber[] complexColumn = new ComplexNumber[len];
 
@@ -327,9 +330,14 @@ public class DigitalSignalProcessing {
 		return transformed;
 	}
 
+	/**
+	 * This method analyzes the data from 3 tones of dual-tone multi-frequency waves and determines
+	 * which button on the keypad corresponds to the tones.
+	 */
 	private static void DTMFtones() {
 		ArrayList<Double> samples = new ArrayList<Double>();
 
+		// read the tone samples from a file
 		try {
 			BufferedReader in = new BufferedReader(new FileReader("tonedataF20173.txt"));
 			String line;
@@ -347,6 +355,8 @@ public class DigitalSignalProcessing {
 			sampleArray[x] = samples.get(x);
 		}
 
+		// perform the FFT and print the power spectral density to determine
+		// the frequencies of the two waves that compose the tone
 		for (ComplexNumber c: fastFourierTransform(convertToComplex(sampleArray), false)) {
 			System.out.println(Math.pow(c.magnitude(), 2));
 		}
@@ -414,6 +424,15 @@ public class DigitalSignalProcessing {
 		return vector;
 	}
 
+	/**
+	 * Generates a sine wave with 'terms' number of even or odd frequencies and a
+	 * specified number of samples. The resulting wave fits the formula of the
+	 * sum from 1 to 'terms' of sin(2pift)/f where f and even or odd frequency.
+	 * @param terms The number of frequencies to generate.
+	 * @param samples The number of wave samples to generate.
+	 * @param odd Odd frequencies if true. Otherwise even.
+	 * @return The wave samples.
+	 */
 	private static double[] sineFunction(int terms, double samples, boolean odd) {
 		double time = 0;
 		double interval = 1/samples;
@@ -443,9 +462,13 @@ public class DigitalSignalProcessing {
 		return results;
 	}
 
+	/**
+	 * Applies filters to a signal.
+	 */
 	private static void filters() {
 		double[] sineWave = sineFunction(100, 512, false);
 
+		// some filters that allow certain frequencies to pass through
 		double[] lowPass = generateFilter(1, 5, 512);
 		double[] highPass = generateFilter(6, 100, 512);
 		double[] bandPass = generateFilter(4, 7, 512);
@@ -469,6 +492,13 @@ public class DigitalSignalProcessing {
 
 	}
 
+	/**
+	 * Generates an even-frequency filter allowing 'end' - 'start' frequencies to pass.
+	 * @param start The lowest frequency to pass.
+	 * @param end The highest frequency to pass.
+	 * @param len The size that the filter should be.
+	 * @return The filter.
+	 */
 	private static double[] generateFilter(int start, int end, int len) {
 		double[] filter = new double[len];
 
@@ -480,6 +510,12 @@ public class DigitalSignalProcessing {
 		return filter;
 	}
 
+	/**
+	 * A piece-wise function that fits the sine function above with infinite frequencies.
+	 * @param samples The number of samples to take from the piece-wise function.
+	 * @param odd True if the samples should be generated from the odd function. False for even.
+	 * @return
+	 */
 	private static double[] piecewiseFunction(double samples, boolean odd) {
 		double time = 0;
 		double interval = 1/samples;
@@ -514,6 +550,10 @@ public class DigitalSignalProcessing {
 		return results;
 	}
 
+	/**
+	 * Observes the effects of phase shift on power spectral density.
+	 * @param pulseIndex The index at which a pulse should be generated.
+	 */
 	private static void phaseShift(int pulseIndex) {
 		double[] pulseArray = new double[1024];
 
@@ -549,12 +589,22 @@ public class DigitalSignalProcessing {
 
 	}
 
+	/**
+	 * Prints the power spectral density of a fast fourier transform.
+	 * @param transform The transformed data.
+	 * @param values The number of values to print.
+	 */
 	private static void printPowerSpectralDensity(ComplexNumber[] transform, int values) {
 		for (int x=0; x<values; x++) {
 			System.out.println(Math.pow(transform[x].magnitude(), 2));
 		}
 	}
 
+	/**
+	 * Converts a double array to an array of complex numbers.
+	 * @param data The array to be converted.
+	 * @return Complex versions of the data.
+	 */
 	private static ComplexNumber[] convertToComplex(double[] data) {
 		ComplexNumber[] converted = new ComplexNumber[data.length];
 
@@ -565,6 +615,10 @@ public class DigitalSignalProcessing {
 		return converted;
 	}
 
+	/**
+	 * Runs unit tests on classes and methods associated with this project.
+	 * @param enabled Tests should only be run if this is set to true.
+	 */
 	private static void runTests(boolean enabled) {
 		if (enabled) {
 			System.out.println("\tComplex Number Tests");
@@ -576,6 +630,9 @@ public class DigitalSignalProcessing {
 		
 	}
 
+	/**
+	 * Tests for fast fourier transform-related methods.
+	 */
 	private static void FFTtest() {
 		double[] testData = {26160, 19011, 18757, 18405, 17888, 14720, 14285, 17018,
 				 			 18014, 17119, 16400, 17497, 17846, 15700, 17636, 17181};
@@ -627,6 +684,12 @@ public class DigitalSignalProcessing {
 
 	}
 
+	/**
+	 * Compare the results of an inverse FFT to the original input data.
+	 * @param complexTestData The original test data.
+	 * @param inverse The inverted FFT data.
+	 * @return True if the data matches its original state.
+	 */
 	private static boolean FFTresultsCheck(ComplexNumber[] complexTestData, ComplexNumber[] inverse) {
 
 		for (int i=0; i<complexTestData.length; i++) {
