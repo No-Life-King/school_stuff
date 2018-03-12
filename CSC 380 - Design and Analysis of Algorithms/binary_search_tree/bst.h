dbfb14528ed471dea24e471ecffca8213cc0b3b0
Unknown <nolife.king88@gmail.com>
2018-03-12 07:41:05 -0400

    Finished and Commented BST Authentication System

--- CSC 380 - Design and Analysis of Algorithms/binary_search_tree/bst.h
+++ CSC 380 - Design and Analysis of Algorithms/binary_search_tree/bst.h
@@ -9,7 +9,6 @@
 #ifndef BINARY_SEARCH_TREE_BST_H
 #define BINARY_SEARCH_TREE_BST_H
 
-#endif //BINARY_SEARCH_TREE_BST_H
 
 typedef struct tree_node {
     char *username;
@@ -25,13 +24,20 @@ typedef struct {
     TreeNode *root;
 } BST;
 
+
 BST *init_tree(char *usn, char *pass);
 TreeNode *make_node(char *usn, char *pass);
 bool add(BST *tree, char *usn, char *pass);
-int compare(const char *u1, const char *u2);
+int compare(const char *usn, const char *node_usn);
 void print_tree(BST *tree);
 void print_node(TreeNode *node);
 
+/**
+ * Initialize a binary search tree with a root node containing the provided username and password.
+ * @param usn A string containing the user's username.
+ * @param pass A string containing the user's password.
+ * @return A pointer to the binary search tree.
+ */
 BST *init_tree(char *usn, char *pass) {
     BST *tree = malloc(sizeof(BST));
     tree->count = 1;
@@ -40,8 +46,22 @@ BST *init_tree(char *usn, char *pass) {
     return tree;
 }
 
+/**
+ * Create a new node to be inserted into the binary search tree.
+ * @param usn A string containing the user's username.
+ * @param pass A string containing the user's password.
+ * @return A pointer to the tree node.
+ */
 TreeNode *make_node(char *usn, char *pass) {
     TreeNode *node = malloc(sizeof(TreeNode));
+
+    // if malloc ends up failing because of too many tree nodes or not enough memory, let me know
+    if (node == NULL) {
+        puts("malloc failed\n");
+        return NULL;
+    }
+
+    // initialize node values
     node->username = usn;
     node->password = pass;
     node->left_child = NULL;
@@ -50,73 +70,120 @@ TreeNode *make_node(char *usn, char *pass) {
     return node;
 }
 
+/**
+ * Create a new user and add him or her to the tree.
+ * @param tree A pointer to the tree to which the user should be added.
+ * @param usn A string containing the user's username.
+ * @param pass A string containing the user's password.
+ * @return True if the new user has chosen a unique username. Otherwise false.
+ */
 bool add(BST *tree, char *usn, char *pass) {
     TreeNode *current = tree->root;
     int current_height = 0;
 
+    // find the user's place in the tree
     while (true) {
-
+        // compare the username with the username at each node
         int comparison = compare(usn, current->username);
+
+        // keep track of the current height and if the tree has reached a new height, increment its height value
         if (comparison > 0 || comparison < 0) {
             current_height++;
             if (current_height > tree->height) {
                 tree->height++;
             }
+        } else {
+            // return false if the username is already in the tree
+            return false;
         }
 
+        // if the username appears before the username of the current node in alphabetical order, either descend the
+        // left tree of the node or insert it as a child of the node if it doesn't have one already
         if (comparison == -1) {
             if (current->left_child == NULL) {
                 current->left_child = make_node(usn, pass);
+
+                // check for malloc fail
+                if (current->left_child == NULL) {
+                    printf("Height:\t%i\nCount:\t%i\n", tree->height, tree->count);
+                }
+
                 tree->count++;
 
+                // watch the insertion operations if necessary
                 //printf("%s is now the left child of %s\n", usn, current->username);
 
                 return true;
             } else {
                 current = current->left_child;
             }
-        } else if (comparison == 1) {
+        }
+            // if the username appears after the username of the current node in alphabetical order, either descend the
+            // right tree of the node or insert it as a child of the node if it doesn't have one already
+        else if (comparison == 1) {
             if (current->right_child == NULL) {
                 current->right_child = make_node(usn, pass);
+
+                // check for malloc fail
+                if (current->right_child == NULL) {
+                    printf("Height:\t%i\nCount:\t%i\n", tree->height, tree->count);
+                }
                 tree->count++;
 
+                // watch the insertion operations if necessary
                 //printf("%s is now the right child of %s\n", usn, current->username);
 
                 return true;
             } else {
                 current = current->right_child;
             }
-        } else {
-            return false;
         }
-
     }
 }
 
-int compare(const char *u1, const char *u2) {
+/**
+ * Compare two usernames to see which appears first in alphabetical order.
+ * @param usn A string containing the passed-in username.
+ * @param node_usn A string containing the username of the current node.
+ * @return 1 if the username is lower than the node username. 0 if the usernames are equal. -1 if the username is
+ * higher than the node username.
+ */
+int compare(const char *usn, const char *node_usn) {
     int index = 0;
 
-    while (u1[index] != '\0' && u2[index] != '\0') {
-        if (u1[index] < u2[index]) {
+    // compare until one of the ends is reached
+    while (usn[index] != '\0' && node_usn[index] != '\0') {
+        if (usn[index] < node_usn[index]) {
             return -1;
-        } else if (u1[index] > u2[index]) {
+        } else if (usn[index] > node_usn[index]) {
             return 1;
         }
         index++;
     }
 
-    if (u1[index] == '\0' && u2[index] == '\0') {
+    // if both ends are reached at the the same time, the usernames are equal
+    if (usn[index] == '\0' && node_usn[index] == '\0') {
         return 0;
     }
-    if (u2[index] == '\0') {
+
+    // if the username contains the node username but is longer, it should appear after the node
+    if (node_usn[index] == '\0') {
         return 1;
     }
-    if (u1[index] == '\0') {
+
+    // if the node username contains the username but is longer, the username should appear before the node
+    if (usn[index] == '\0') {
         return -1;
     }
 }
 
+/**
+ * Print every username in the binary search tree.
+ * @param tree A pointer to the tree that should be printed.
+ */
 void print_tree(BST *tree) {
+
+    // check if the tree is empty. If not, pass the root node to the print_node function
     if (tree->root != NULL) {
         print_node(tree->root);
     } else {
@@ -124,11 +191,20 @@ void print_tree(BST *tree) {
     }
 }
 
+/**
+ * Given a starting node, use a recursive in-order method to print the tree. Amazingly, the usernames will be
+ * printed in alphabetical order.
+ * @param node The node to be printed.
+ */
 void print_node(TreeNode *node) {
+
+    // when a node has no left and/or right child, NULL will be passed into this function which is where it stops
+    // recursing
     if (node != NULL) {
         print_node(node->left_child);
-        printf("%s\n", node->username);
+        printf("%30s  %-30s\n", node->username, node->password);
         print_node(node->right_child);
     }
 }
 
+#endif //BINARY_SEARCH_TREE_BST_H
--- CSC 380 - Design and Analysis of Algorithms/binary_search_tree/main.c
+++ CSC 380 - Design and Analysis of Algorithms/binary_search_tree/main.c
@@ -1,76 +1,107 @@
 #include <stdio.h>
+#include <windows.h>
 #include "bst.h"
 
-#define NUM_ACCTS   500000
+// set the number of accounts to be read in and inserted into the tree here
+#define NUM_ACCTS   1000000
 
 int str_len(const char *string);
 char **read_in_accounts(int num_to_read);
 char *lowercase(const char *username);
+char *get_password_pointer(char *usn);
+double getTime();
 
 void main() {
-
+    // read in the specified number of accounts from a file
     char **accts = read_in_accounts(NUM_ACCTS);
 
-    printf("file read finished");
+    // initialize the binary search tree
+    char *usn = accts[0];
+    char *pass = get_password_pointer(usn);
+    BST *tree = init_tree(lowercase(accts[0]), pass);
 
-    BST *tree = init_tree(lowercase(accts[0]), accts[1]);
+    double timer = 0, start, finish;
 
-    for (int x=1; x<NUM_ACCTS; x++) {
-        add(tree, lowercase(accts[2*x]), accts[2*x+1]);
-        free(accts[2*x]);
-        free(accts[2*x+1]);
+    // add the specified number of accounts to the binary search tree
+    for (int x=0; x<NUM_ACCTS; x++) {
+        usn = accts[x];
+
+        char *usn_ptr = lowercase(usn);
+        char *pass_ptr = get_password_pointer(usn);
+        start = getTime();
+        add(tree, usn_ptr, pass_ptr);
+        finish = getTime();
+        timer += finish - start;
     }
 
-
+    // you can print the tree (in sorted order) if the node count is low enough to be printed
     //print_tree(tree);
 
-    printf("\n%i\n", tree->count);
-    printf("\n%i\n", tree->height);
+    printf("\nNode Count:%i\nTree Height: %i\n", tree->count, tree->height);
+    printf("\nTotal Insertion Time: %f s", timer);
 }
 
+/**
+ * Accepts a line from the 'accts' array and returns a pointer to the password.
+ * @param usn A tab separated line that contains a username and password pair.
+ * @return A pointer to the password.
+ */
+char *get_password_pointer(char *usn) {
+    int i = 0;
+    while (usn[i] != '\t') {
+        i++;
+    }
+    return &usn[i+1];
+}
+
+/**
+ * Read lines containing login information into a jagged 2D array. This should use about the smallest amount of memory
+ * possible without compressing data.
+ * @param num_to_read The number of accounts to read in from the file.
+ * @return A pointer to an array of char array pointers.
+ */
 char **read_in_accounts(int num_to_read) {
+
+    // open file
     FILE *fp;
     fp = fopen("D:\\Devel\\school_stuff\\CSC 380 - Design and Analysis of Algorithms\\username_generator\\10M_logins.txt", "r");
-    char *line = NULL;
+
+    // initialize array of accounts
     char **lines;
-    lines = (char **)malloc(sizeof(char *) * num_to_read * 2);
+    lines = (char **) malloc(sizeof(char *) * num_to_read);
+
+    // set up line reading variables
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
     int lineNumber = 0;
 
+    // read lines into the accounts array until you have enough or hit the end of the file
     while ((read = getline(&line, &len, fp)) != EOF && lineNumber < num_to_read) {
 
-        int len_usn = 0;
-        while (line[len_usn] != '\t') {
-            len_usn++;
-        }
+        // define the size of the line
+        lines[lineNumber] = (char *) malloc((size_t) read);
 
-        lines[lineNumber * 2] = (char *) malloc((size_t) len_usn+1);
-        lines[lineNumber * 2][len_usn] = '\0';
-
-        int i = 0;
-        while (i < len_usn) {
-            lines[lineNumber * 2][i] = line[i];
+        // copy the contents of the line into the array
+        int i=0;
+        while (i<read-1) {
+            lines[lineNumber][i] = line[i];
             i++;
         }
 
-        int len_pass = read - len_usn - 1;
-        lines[lineNumber * 2 + 1] = (char *) malloc((size_t) len_pass);
-        int j = 0;
-        len_usn++;
-        while (len_usn < read-1) {
-            lines[lineNumber * 2 + 1][j] = line[len_usn];
-            len_usn++;
-            j++;
-        }
-
-        lines[lineNumber*2+1][len_pass-1] = '\0';
+        // replace newline with null character
+        lines[lineNumber][i] = '\0';
         lineNumber++;
     }
 
     return lines;
 }
 
+/**
+ * Count the number of characters in a string, excluding the null character.
+ * @param string A string whose characters should be counted.
+ * @return The number of characters in the string.
+ */
 int str_len(const char *string) {
     int i = 0;
     while (string[i] != '\0') {
@@ -80,11 +111,23 @@ int str_len(const char *string) {
     return i;
 }
 
+/**
+ * Accepts a string and converts all uppercase letters to lowercase. Allocates space for a new string rather than
+ * modifying it in-place.
+ * @param username The string to be converted to lowercase.
+ * @return A lowercase version of the string.
+ */
 char *lowercase(const char *username) {
+
+    // allocate space just large enough for the lowercased username
     char *lc = malloc((size_t) str_len(username));
+
+    // iterate through the username copying the lowercased version to the new username string
     int i = 0;
     char current = username[i];
-    while (current != '\0') {
+    while (current != '\t') {
+
+        // if capital letter, convert to lower before copying
         if (64 < current && current < 91) {
             current += 32;
         }
@@ -93,6 +136,14 @@ char *lowercase(const char *username) {
         i++;
         current = username[i];
     }
+
     lc[i++] = '\0';
     return lc;
+}
+
+double getTime() {
+    LARGE_INTEGER t, f;
+    QueryPerformanceCounter(&t);
+    QueryPerformanceFrequency(&f);
+    return (double)t.QuadPart/(double)f.QuadPart;
 }
